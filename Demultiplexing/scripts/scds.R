697d722d9e054fa2e50697e43fee0fe3f8d31a3c
drneavin <d.neavin@garvan.org.au>
2020-09-10 00:02:39 +1000

    fixed scds error

--- Demultiplexing/scripts/scds.R
+++ Demultiplexing/scripts/scds.R
@@ -16,20 +16,29 @@ tenX <- arguments[3,]
 counts <- Read10X(as.character(tenX[1]), gene.column = 1)
 sce <- SingleCellExperiment(list(counts=counts))
 
-## Annotate doublet using co-expression based doublet scoring:
-sce = cxds(sce, retRes = TRUE)
-
 ## Annotate doublet using binary classification based doublet scoring:
-sce = bcds(sce, retRes = TRUE)
+sce = bcds(sce, retRes = TRUE, estNdbl=TRUE)
 
-## Combine both annotations into a hybrid annotation
-sce = cxds_bcds_hybrid(sce, estNdbl=TRUE)
+## Annotate doublet using co-expression based doublet scoring:
+try({
+    sce = cxds(sce, retRes = TRUE, estNdbl=TRUE)
+})
+
+### If cxds worked, run hybrid, otherwise use bcds annotations
+if ("cxds_score" %in% colnames(colData(sce))) {
+    ## Combine both annotations into a hybrid annotation
+    sce = cxds_bcds_hybrid(sce, estNdbl=TRUE)
+    Doublets <- as.data.frame(cbind(rownames(colData(sce)), colData(sce)$hybrid_score, colData(sce)$hybrid_call))
+} else {
+    print("this pool failed cxds so results are just the bcds calls")
+    Doublets <- as.data.frame(cbind(rownames(colData(sce)), colData(sce)$bcds_score, colData(sce)$bcds_call))
+}
 
 ## Doublet scores are now available via colData:
-Doublets <- as.data.frame(cbind(rownames(colData(sce)), colData(sce)$hybrid_score, colData(sce)$hybrid_call))
 colnames(Doublets) <- c("Barcode","scds_score","scds_DropletType")
 Doublets$scds_DropletType <- gsub("FALSE","singlet",Doublets$scds_DropletType) 
 Doublets$scds_DropletType <- gsub("TRUE","doublet",Doublets$scds_DropletType)
 
 message("writing output")
 write_delim(Doublets, paste0(out,"/scds_doublets.txt"), "\t")
+
