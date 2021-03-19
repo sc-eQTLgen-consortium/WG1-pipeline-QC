4e814ec1465b174d89aeeff601beaaa26d090eef
drneavin <d.neavin@garvan.org.au>
2021-03-19 12:49:39 +1100

    updates to fix path bugs

--- Demultiplexing/mods/prepareArguments.py
+++ Demultiplexing/mods/prepareArguments.py
@@ -23,21 +23,52 @@ def get_barcodes_files(pool_dir):
             if re.search(r'filtered', os.path.join(dirpath, filename)):
                 return(os.path.join(dirpath, filename))
 
+### Get the barcode files for each pool     
+def get_barcodes_dir(scrnaseq_filelist, pools = None):    
+    try:
+        barcode_filelist = [get_barcodes_files(pool) for pool in scrnaseq_filelist]
+        barcode_filedict = dict(zip(pools, barcode_filelist))
+        barcode_libs = pd.Series(barcode_filedict, name="Barcode_Files")
+        return(barcode_libs)
+    except Exception as error:
+        print(error)
+        raise SystemExit("Could not find a barcode file in all the scRNA-seq pool directories. Please check that they exist somewhere in your pool scRNA-seq directories and contain 'barcodes.tsv' within the name.")
+    
 def get_bam_files(pool_dir):
     for dirpath, dirnames, filenames in os.walk(pool_dir):
         for filename in [f for f in filenames if f.endswith(".bam")]:
             return(os.path.join(dirpath, filename))
 
+def get_bam_dirs(scrnaseq_filelist, pools = None):
+    try:
+        bam_filelist = [get_bam_files(pool) for pool in scrnaseq_filelist]
+        bam_filedict = dict(zip(pools, bam_filelist))
+        bamlibs = pd.Series(bam_filedict, name="Bam_Files")
+        return(bamlibs)
+    except Exception as error:
+        print(error)
+        raise SystemExit("Could not find a bam file in all the scRNA-seq pool directories. Please check that they exist somewhere in your pool scRNA-seq directories and contain '.bam' within the name.")
+    
 def get_matrix_files(pool_dir):
     for dirpath, dirnames, filenames in os.walk(pool_dir):
         for filename in [f for f in filenames if re.search("matrix.mtx", f)]:
             if re.search(r'filtered', os.path.join(dirpath, filename)):
                 return(os.path.join(dirpath, filename))
-def get_matrix_dirs(pool_dir):
-    for dirpath, dirnames, filenames in os.walk(pool_dir):
-        for filename in [f for f in filenames if re.search("matrix.mtx", f)]:
-            if re.search(r'filtered', os.path.join(dirpath, filename)):
-                return(os.path.join(dirpath))
+
+def get_matrix_dirs(matrix_libs, pools = None):
+
+    try:
+        matrix_filelist = [get_matrix_files(pool) for pool in scrnaseq_filelist]
+        matrix_filedict = dict(zip(pools, matrix_filelist))
+        matrix_libs = pd.Series(matrix_filedict, name="Matrix_Files")
+        matrix_dirlist = [os.path.dirname(filename) for filename in matrix_filelist]
+        matrix_dirdict = dict(zip(pools, matrix_dirlist))
+        matrix_dir_libs = pd.Series(matrix_dirdict, name="Matrix_Directories")
+        return(matrix_dir_libs, matrix_libs)
+    except Exception as error:
+        print(error)
+        raise SystemExit("Could not find a matrix file in all the scRNA-seq pool directories. Please check that they exist somewhere in your pool scRNA-seq directories and contain 'matrix.mtx' within the name.")
+        
 
 def get_individual_files(x, individual_dir = None):
     for filename in individual_dir:
@@ -50,7 +81,17 @@ def get_individual_files(x, individual_dir = None):
         elif re.search(x + '\D', filename):
             return(filename)
 
-
+def get_individual_dirs(individual_list_dir, pools = None):
+    individual_dirlist = os.listdir(individual_list_dir)
+    try:
+        individual_filelist = [os.path.join(individual_list_dir, get_individual_files(pool, individual_dir = individual_dirlist)) for pool in pools]
+        individual_filedict = dict(zip(pools, individual_filelist))
+        individual_libs = pd.Series(individual_filedict, name="Individuals_Files")
+        return(individual_libs)
+    except Exception as error:
+        print(error)
+        raise SystemExit("Could not find a files of individuals in {}. Please check that they exist somewhere in this directory and contain the pool names within the name of the file.".format(individual_list_dir))
+    
 def get_scrnaseq_dirs(config):
     # Extract variables from configuration file for use within the rest of the pipeline
     input_dict = config["inputs"]
@@ -107,10 +148,7 @@ def get_scrnaseq_dirs(config):
     matrix_libs = pd.Series(matrix_filedict, name="Matrix_Files")
 
     ### Get the directories of all the matrix EXCLUDING the matrix filenames themselves
-    try:
-        matrix_dirlist = [get_matrix_dirs(pool) for pool in scrnaseq_filelist]
-    except:
-        print("Could not find a matrix file in all the scRNA-seq pool directories. Please check that they exist somewhere in your pool scRNA-seq directories and contain 'matrix.mtx' within the name.")
+    matrix_dirlist = [os.path.dirname(pool) for pool in matrix_filelist]
     matrix_dirdict = dict(zip(pools, matrix_dirlist))
     matrix_dir_libs = pd.Series(matrix_dirdict, name="Matrix_Directories")
 
@@ -126,3 +164,10 @@ def get_scrnaseq_dirs(config):
     dataframe=pd.concat([scrnaseq_libs, barcode_libs, bamlibs, matrix_libs, matrix_dir_libs, individual_libs], axis=1)
     return(dataframe)
 
+def parsePaths(config):
+    # Strip trailing slashes from paths 
+    # scrnaseq_dir, individual_list_dir, output_dir
+    config["inputs"]["scRNAseq_dir"] = (config["inputs"]["scRNAseq_dir"]).rstrip("/")
+    config["inputs"]["individual_list_dir"] = (config["inputs"]["individual_list_dir"]).rstrip("/")
+    config["outputs"]["output_dir"] = (config["outputs"]["output_dir"]).rstrip("/")
+    return(config)
