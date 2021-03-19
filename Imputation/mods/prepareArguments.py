75103e1211e5f5519240f2e4e2d26406428b64a7
drneavin <d.neavin@garvan.org.au>
2021-03-19 13:33:04 +1100

    Add path parsing and snakemake conda environment

---
+++ Imputation/mods/prepareArguments.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import os
+import pandas as pd
+from glob import glob
+import re
+import subprocess
+
+def parsePaths(config):
+    # Strip trailing slashes from paths 
+    # scrnaseq_dir, individual_list_dir, output_dir
+    config["inputs"]["scRNAseq_dir"] = (config["inputs"]["scRNAseq_dir"]).rstrip("/")
+    return(config) 
